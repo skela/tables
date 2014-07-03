12c6da8b099ff9579aad9bb44dcabb5de6af7259
Alek Slater <slayte@gmail.com>
2014-07-03 23:20:09 +0800

    added simple table support for android

--- Mono/Tables.Droid/TableAdapterCell.cs
+++ Mono/Tables.Droid/TableAdapterCell.cs
@@ -7,14 +7,26 @@ using Android.Views;
 
 namespace Tables.Droid
 {
-    public interface ITableAdapterCell
+    public interface ITableAdapterSimpleCell
     {
         TextView Title { get; }
         TextView Detail { get; }
+        bool Editable {set;}
+    }
+
+    public interface ITableAdapterCell : ITableAdapterSimpleCell
+    {
         TextView Blurb { get; }
         CheckBox Switch { get; }
+    }
 
-        bool Editable {set;}
+    public static class TableUtils
+    {
+        public static int GetPixelsFromDPI(Context ctx, int dpi)
+        {
+            int pixels = (int) TypedValue.ApplyDimension(ComplexUnitType.Dip,dpi,ctx.Resources.DisplayMetrics);
+            return pixels;
+        }
     }
 
     public class TableAdapterCell : LinearLayout,ITableAdapterCell
@@ -67,7 +79,7 @@ namespace Tables.Droid
             AddView(Blurb);
 
             Orientation = Orientation.Vertical;
-            int pixels = GetPixelsFromDPI(context, 10);
+            int pixels = TableUtils.GetPixelsFromDPI(context, 10);
             SetPadding(pixels, pixels, pixels, pixels);
         }
 
@@ -78,12 +90,57 @@ namespace Tables.Droid
                 Focusable = !value;
             }
         }
+    }
 
+    public class TableAdapterSimpleCell : LinearLayout,ITableAdapterSimpleCell
+    {
+        public TextView Title { get; private set;}
+        public TextView Detail { get; private set;}
 
-        public static int GetPixelsFromDPI(Context ctx, int dpi)
+        public TableAdapterSimpleCell(Context context) : base(context)
         {
-            int pixels = (int) TypedValue.ApplyDimension(ComplexUnitType.Dip,dpi,ctx.Resources.DisplayMetrics);
-            return pixels;
+            Prepare(context);
+        }
+
+        public TableAdapterSimpleCell (Context context, IAttributeSet attrs) : base (context, attrs)
+        {
+            Prepare (context);
+        }
+
+        private void Prepare(Context context)
+        {
+            LinearLayout line = new LinearLayout(context);
+            {
+                line.SetVerticalGravity(GravityFlags.Center);
+                line.Orientation = Orientation.Horizontal;
+
+                Title= new TextView(Context);
+                Title.Text = "Title";
+                Title.Typeface = Android.Graphics.Typeface.DefaultBold;
+                line.AddView(Title);
+
+                View filler = new View(context);
+                filler.LayoutParameters = new LinearLayout.LayoutParams(Android.Widget.LinearLayout.LayoutParams.WrapContent, Android.Widget.LinearLayout.LayoutParams.WrapContent, 1f);
+
+                line.AddView(filler);
+            }
+            AddView(line);
+
+            Detail = new TextView(context);
+            Detail.Text = "Detail";
+            AddView(Detail);
+
+            Orientation = Orientation.Vertical;
+            int pixels = TableUtils.GetPixelsFromDPI(context, 10);
+            SetPadding(pixels, pixels, pixels, pixels);
+        }
+
+        public bool Editable 
+        {
+            set
+            {
+                Focusable = !value;
+            }
         }
     }
 }
